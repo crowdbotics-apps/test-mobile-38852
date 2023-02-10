f6be97c9e5fd0519359c5f9c90d16df3af2cf5a2
Nikos Kazantzis <nikos.kazantzis@hellenic-technologies.gr>
2023-02-11 00:24:01 +0200

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "test-mobile-38852",
   url: "https://test-mobile-38852.botics.co",
   api: "https://test-mobile-38852.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
